e403cf1feb550aa0df085ab0575b79a6b20f8374
DaniilKozlov <enjoy2k@mail.ru>
2022-07-29 12:04:18 +0300

    Написал заметку

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel! // IBOutlet Это связать элемента интерфейса "hello, world" с переменной "helloWorldLabel". В перечне строк появляется кружок, наведя курсор на который, подсвечивается элемент интерфейса с которым связана эта строка кода
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
